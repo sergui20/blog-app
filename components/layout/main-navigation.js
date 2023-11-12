af6cf9414c661b48796fb5c4151473298e9d8c85
Sergui Morejon <serguinajibm20@gmail.com>
2023-11-11 22:30:21 -0500

    Fourth lesson: Adding Layout and Navigation

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,3 +1,12 @@
+/**
+ * 4.2: Adding Layout and Navigation.
+ * For the links we will not use the standard "<a>" tag, because that would 
+ * be handled by the browser only, and when we click it, we would load a brand-new page 
+ * and send a brand-new request. Instead, we want to use Next's Link component 
+ * to take advantage of automatic data prefetching which it does for us, 
+ * and to take advantage of the fact that we stay in a single-page application 
+ * once the page was loaded initially.
+ */
 import Link from 'next/link';
 
 import Logo from './logo';
@@ -6,12 +15,20 @@ import classes from './main-navigation.module.css';
 function MainNavigation() {
   return (
     <header className={classes.header}>
+      {/* 4.4: Adding Layout and Navigation.  
+        You might remember that when you pass a component as children, Link will not
+        render the "<a>" tag, so we need to explicitly define it. This is no londer needed
+        for the latest version of Next.js though.
+      */}
       <Link href='/'>
         <a>
           <Logo />
         </a>
       </Link>
       <nav>
+        {/* 4.3: Adding Layout and Navigation. 
+          The Links you see below matches the structure of the routes of this page.
+        */}
         <ul>
           <li>
             <Link href='/posts'>Posts</Link>
--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,12 @@
+/**
+ * 4.1: Adding Layout and Navigation.
+ * Let's add a general layout and a navigation bar, because it will not only 
+ * be needed on the homepage.
+ * We wrapped our entire app with the "Layout" component you can see below.
+ * The way we split or built our layout is by creating a "layout" subfolder
+ * under the "components" folder. And in there add the components we need for the layout
+ * such as "logo" and "main-navigation".
+ */
 import Head from 'next/head';
 
 import '../styles/globals.css';
